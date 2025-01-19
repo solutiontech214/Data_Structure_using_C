2ef503b56b0b9a92b53cd4c928e903e81112b38a
solutiontech214 <onkargutti94@gmail.com>
2025-01-19 20:27:10 +0530

    s0ke

---
+++ fact.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter a number:");
+scanf("%d",&num);
+int fact=1;
+while(num>=1)
+{
+fact=fact*num;
+num--;
+}
+printf("factorial is :%d",fact);
+}
