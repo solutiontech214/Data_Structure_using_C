0cc0be7f38ab989f11f083583a5931453199384a
solutiontech214 <onkargutti94@gmail.com>
2025-01-26 20:33:05 +0530

    insertion sort

--- insertionSort.c
+++ insertionSort.c
@@ -1,3 +1,14 @@
+// Created by Onkar Gutti on 26/1/2025
+
+//insertion sort is a simple sorting algorithm that works the way we sort playing cards in our hands.
+//Insertion sort is a simple sorting algorithm that works the way we sort playing cards in our hands. The array is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part.
+//Algorithm
+//To sort an array of size n in ascending order:
+//1: Iterate from arr[1] to arr[n] over the array.
+//2: Compare the current element (key) to its predecessor.
+//3: If the key element is smaller than its predecessor, compare it to the elements before. Move the greater elements one position up to make space for the swapped element.
+//The above code is for insertion sort. The code below is for selection sort. The code is similar to the above code. The only difference is the sorting algorithm used. The above code uses insertion sort and the below code uses selection sort. The code below is for selection sort. The code is similar to the above code. The only difference is the sorting algorithm used. The above code uses insertion sort and the below code uses selection sort. The code below is for selection sort. The code is similar to the above code. The only difference is the sorting algorithm used. The above code uses insertion sort and the below code uses selection sort. The code below is for selection sort. The code is similar to the above code. The only difference is the sorting algorithm used. The above code uses insertion sort and the below code uses selection sort.
+
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
